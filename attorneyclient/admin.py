d57ea630d712e5f038933fc4c053def3cdd5a18e
V David Zvenyach <vdavez@gmail.com>
2014-10-10 20:50:45 -0400

    stubbing out the model, cleaning up the directory

--- attorneyclient/admin.py
+++ attorneyclient/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from attorneyclient.models import Client
+from attorneyclient.models import Client, Record, Form
 
 admin.site.register(Client)
+admin.site.register(Record)
+admin.site.register(Form)
--- attorneyclient/models.py
+++ attorneyclient/models.py
@@ -1,7 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Form(models.Model):
+    form_name = models.CharField(max_length=200)
+
+class FormField(models.Model):
+    form = models.ForeignKey(Form)
+
+class Record(models.Model):
+    attorneys = models.ManyToManyField(User)
+    client = models.ManyToManyField(Client)
+    form = models.ForeignKey(Form)
+
+class RecordField(models.Model):
+    record = models.ForeignKey(Record)
