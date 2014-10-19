37293778c8c3bfb33bccef08576f2758dbe3bd00
V David Zvenyach <vdavez@gmail.com>
2014-10-19 15:41:23 -0400

    templates and calling it a day

--- tenacious/urls.py
+++ tenacious/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
+
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url('', include('django.contrib.auth.urls')),
     url(r'^$', 'tenacious.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^start/', include('attorneyclient.urls')),
